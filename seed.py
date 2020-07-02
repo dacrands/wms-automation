43e9448031784615b7657f2fd7758e0a4e9b0f46
dacrands <davecrands@gmail.com>
2020-07-02 16:14:56 -0500

    Add seed file

---
+++ seed.py
@@ -0,0 +1,62 @@
+newItemsList = [
+    {
+        'txtCompany': 'test-0',
+        'txtCompanyName': 'Test Company 0',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+     {
+        'txtCompany': 'test-0',
+        'txtCompanyName': 'Test Company 1',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+     {
+        'txtCompany': 'test-2',
+        'txtCompanyName': 'Test Company 2',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+     {
+        'txtCompany': 'test-3',
+        'txtCompanyName': 'Test Company 3',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+     {
+        'txtCompany': 'test-4',
+        'txtCompanyName': 'Test Company 4',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+     {
+        'txtCompany': 'test-4',
+        'txtCompanyName': 'Test Company 4',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+]
