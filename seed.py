bdfd09e128c14c68f493c2b49b2ce0248290bed5
dacrands <davecrands@gmail.com>
2020-07-08 14:39:45 -0500

    Add more seed data

--- seed.py
+++ seed.py
@@ -9,7 +9,7 @@ newItemsList = [
         'txtZip': '99999',
         'txtTaxRate': '9'
     },
-     {
+    {
         'txtCompany': 'test-0',
         'txtCompanyName': 'Test Company 1',
         'txtType': 'V',
@@ -19,7 +19,7 @@ newItemsList = [
         'txtZip': '99999',
         'txtTaxRate': '9'
     },
-     {
+    {
         'txtCompany': 'test-2',
         'txtCompanyName': 'Test Company 2',
         'txtType': 'V',
@@ -29,7 +29,7 @@ newItemsList = [
         'txtZip': '99999',
         'txtTaxRate': '9'
     },
-     {
+    {
         'txtCompany': 'test-3',
         'txtCompanyName': 'Test Company 3',
         'txtType': 'V',
@@ -39,7 +39,7 @@ newItemsList = [
         'txtZip': '99999',
         'txtTaxRate': '9'
     },
-     {
+    {
         'txtCompany': 'test-4',
         'txtCompanyName': 'Test Company 4',
         'txtType': 'V',
@@ -49,9 +49,129 @@ newItemsList = [
         'txtZip': '99999',
         'txtTaxRate': '9'
     },
-     {
+    {
         'txtCompany': 'test-4',
         'txtCompanyName': 'Test Company 4',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-5',
+        'txtCompanyName': 'Test Company 5',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-6',
+        'txtCompanyName': 'Test Company 6',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-7',
+        'txtCompanyName': 'Test Company 7',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-7',
+        'txtCompanyName': 'Test Company 7',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-8',
+        'txtCompanyName': 'Test Company 8',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-9',
+        'txtCompanyName': 'Test Company 9',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-9',
+        'txtCompanyName': 'Test Company 9',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-10',
+        'txtCompanyName': 'Test Company 10',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-11',
+        'txtCompanyName': 'Test Company 11',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-12',
+        'txtCompanyName': 'Test Company 12',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-13',
+        'txtCompanyName': 'Test Company 13',
+        'txtType': 'V',
+        'txtAddress': '9999 Test Drive',
+        'txtCity': 'Testtown',
+        'txtState': 'Teststate',
+        'txtZip': '99999',
+        'txtTaxRate': '9'
+    },
+    {
+        'txtCompany': 'test-14',
+        'txtCompanyName': 'Test Company 14',
         'txtType': 'V',
         'txtAddress': '9999 Test Drive',
         'txtCity': 'Testtown',
